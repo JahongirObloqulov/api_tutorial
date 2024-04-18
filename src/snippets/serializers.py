866112821d1648de8c28c54bafaca2fe21ecc8fd
JahongirObloqulov <jahongir.obloqulov13@gmail.com>
2024-04-18 18:09:15 +0500

    serializers added

---
+++ src/snippets/serializers.py
@@ -0,0 +1,8 @@
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
---
+++ src/snippets/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('api/', views.SnippetList.as_view()),
+    path('api<int:pk>', views.SnippetDetail.as_view())
+]
